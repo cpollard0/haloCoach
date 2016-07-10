cbee84b6641737b39da19ea8de6bc24271e2a72e
Chris <chris.pollard@rocketmail.com>
2016-07-10 19:57:15 -0400

    cleaning up merge issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 
-import {About} from './components/about/about';
+
 import {Home} from './components/home/home';
-import {RepoBrowser} from './components/repo-browser/repo-browser';
-import {RepoList} from './components/repo-list/repo-list';
-import {RepoDetail} from './components/repo-detail/repo-detail';
 import {SeasonDropdown} from './components/metadata/season-dropdown/season-dropdown'
 import {ArenaStats} from './components/stats/service-record/arena-stats'
 
@@ -12,15 +9,8 @@ const routes: RouterConfig = [
   { path: '', redirectTo: 'home', terminal: true },
   { path: 'home', component: Home },
   {path: 'arena-stats', component: ArenaStats},
-  { path: 'season-dropdown', component:SeasonDropdown},
-  { path: 'about', component: About },
-  { path: 'github', component: RepoBrowser, children: [
-    { path: ':org', component: RepoList, children: [
-      { path: ':repo', component: RepoDetail },
-      { path: '', component: RepoDetail }
-    ]},
-    { path: '', component: RepoList}
-  ]}
+  {path: 'season-dropdown', component: SeasonDropdown}
+
 ];
 
 export const APP_ROUTER_PROVIDERS = [
